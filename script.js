457df4b48280e784a537fb7d722d3f744889c480
jirkapriv <jiri.privratsky@student.spsmb.cz>
2025-02-22 15:23:17 +0100

    mobile resonsivity debugging

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
 const links = document.querySelectorAll("nav a");
 
 links.forEach((link) => {
   link.addEventListener("click", function (event) {
     event.preventDefault();
     const targetId = link.getAttribute("href").substring(1);
-    const targetElement = document.getElementById(targetId); 
+    const targetElement = document.getElementById(targetId);
 
     if (targetElement) {
-      window.scrollTo({ top: targetElement.offsetTop, behavior: "smooth" }); 
+      window.scrollTo({ top: targetElement.offsetTop, behavior: "smooth" });
     } else if (targetId === "top") {
       window.scrollTo({ top: 0, behavior: "smooth" });
     }
